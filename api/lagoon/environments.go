e545ed179fc6e9f10bc3167368cc49491a67ad6b
Chris <chris_2190@hotmail.com>
2023-07-10 18:05:33 +1000

    Initial commit

--- api/lagoon/environments.go
+++ api/lagoon/environments.go
@@ -4,6 +4,7 @@ package lagoon
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/uselagoon/machinery/api/schema"
 )
@@ -18,6 +19,7 @@ type Environments interface {
 	SetEnvironmentServices(ctx context.Context, id uint, services []string, result *[]schema.EnvironmentService) error
 	EnvironmentByName(ctx context.Context, name string, project uint, result *schema.Environment) error
 	EnvironmentByNamespace(ctx context.Context, namespace string, result *schema.Environment) error
+	EnvironmentsByProject(ctx context.Context, project string, result *[]schema.Environment) error
 }
 
 // GetBackupsForEnvironmentByName gets backup info in lagoon for specific environment.
@@ -66,4 +68,11 @@ func GetEnvironmentByName(ctx context.Context, name string, project uint, e Envi
 func GetEnvironmentByNamespace(ctx context.Context, namespace string, e Environments) (*schema.Environment, error) {
 	environment := schema.Environment{}
 	return &environment, e.EnvironmentByNamespace(ctx, namespace, &environment)
+}
+
+// GetEnvironmentsByProjectName lists info of all environments for a given project.
+func GetEnvironmentsByProjectName(ctx context.Context, project string, e Environments) (*[]schema.Environment, error) {
+	environment := []schema.Environment{}
+	fmt.Println(e.EnvironmentsByProject(ctx, project, &environment))
+	return &environment, e.EnvironmentsByProject(ctx, project, &environment)
 }
