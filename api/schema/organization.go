bde140a3340b1ba2025411b0e207362dc33c85b5
cgoodwin90 <chris_2190@hotmail.com>
2023-10-25 12:10:32 +1100

    Fixing up schema

--- api/schema/organization.go
+++ api/schema/organization.go
@@ -16,7 +16,7 @@ type Organization struct {
 type AddOrganizationInput struct {
 	ID                uint   `json:"id,omitempty"`
 	Name              string `json:"name"`
-	FriendlyName      string `json:"friendlyName"`
+	FriendlyName      string `json:"friendlyName,omitempty"`
 	Description       string `json:"description,omitempty"`
 	QuotaProject      int    `json:"quotaProject,omitempty"`
 	QuotaGroup        int    `json:"quotaGroup,omitempty"`
@@ -37,22 +37,22 @@ type OrgProject struct {
 type OrgEnvironment struct {
 	ID                         uint                 `json:"id,omitempty"`
 	Name                       string               `json:"name,omitempty"`
-	ProjectID                  uint                 `json:"project,omitempty"`
+	Project                    OrgProject           `json:"project,omitempty"`
 	DeployType                 DeployType           `json:"deployType,omitempty"`
 	DeployHeadRef              string               `json:"deployHeadRef,omitempty"`
 	DeployTitle                string               `json:"deployTitle,omitempty"`
 	EnvironmentType            EnvType              `json:"environmentType,omitempty"`
 	OpenshiftProjectName       string               `json:"openshiftProjectName,omitempty"`
 	AutoIdle                   uint                 `json:"autoIdle,omitempty"`
-	KubernetesNamespacePattern *string              `json:"kubernetesNamespacePattern,omitempty"`
+	KubernetesNamespacePattern string               `json:"kubernetesNamespacePattern,omitempty"`
 	Updated                    string               `json:"updated,omitempty"`
 	Created                    string               `json:"created,omitempty"`
 	Deleted                    string               `json:"deleted,omitempty"`
 	Route                      string               `json:"route,omitempty"`
 	Routes                     string               `json:"routes,omitempty"`
 	Services                   []EnvironmentService `json:"services,omitempty"`
-	DeployTarget               DeployTarget         `json:"openshift,omitempty"`
-	Kubernetes                 *uint                `json:"kubernetes,omitempty"`
+	Openshift                  DeployTarget         `json:"openshift,omitempty"`
+	Kubernetes                 DeployTarget         `json:"kubernetes,omitempty"`
 }
 
 // AddGroupToOrganizationInput is based on the input to AddGroupToOrganization.
